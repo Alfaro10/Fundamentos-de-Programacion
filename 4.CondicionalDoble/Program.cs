d58f1dd83b33f4e3348e5c1d073c917b78b5b9d2
Alfaro10 <isabel.alfaro@upb.edu.co>
2025-08-13 15:50:26 -0500

    Add files via upload

---
+++ 4.CondicionalDoble/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _4.CondicionalDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y 
+             * el sueldo de una persona, si el sueldo supera los 3000 pesos 
+             * mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. 
+             * De lo contrario mostrar el mensaje “Persona exenta de impuestos”.
+
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if(sueldo>=3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos"); //Verdadera
+            }
+            else
+            {
+                Console.WriteLine($"La persona {nombre}, está exenta de impuestos"); //Falsa
+            }*/
+
+            /*Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, 
+             * se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. 
+             * De lo contrario, mostrar el mensaje “No es apto para el contenido de este sitio web*/
+
+            /*string nombre = "";
+            int edad = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la edad");
+            edad = Int32.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+            else
+            {
+                Console.WriteLine("No es apto para el contenido de este sitio web");
+            }
+
+            /*Crear un algoritmo que permita ingresar dos números, 
+            si el primer número es mayor al segundo número informar su suma y diferencia, 
+            en caso contrario informar el producto y la división del primer número respecto al segundo número.*/
+
+            /*int num1 = 0;
+            int num2 = 0;
+            
+
+            Console.WriteLine("Ingrese el primer número");
+            num1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número");
+            num2 = Int32.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Suma: {num1+num2}, Resta: {num1-num2})");
+            }
+            else
+            {
+                Console.WriteLine($"Producto: {num1*num2}, División: {num1/num2})");
+            }
+            */
+
+            int noches = 0;
+            float descuento = 0f;
+           
+
+            Console.WriteLine("Cuántas noches se hospeda?");
+            noches = Int32.Parse(Console.ReadLine());
+
+            if (noches > 3)
+            {
+                descuento = 0.17f;
+                Console.WriteLine($"Monto a pagar: {200*noches-(200 * noches * descuento)}");
+            }
+            else
+            {
+                descuento = 0.2f;
+                Console.WriteLine($"Monto a pagar: {200 * noches - (200 * noches * descuento)}");
+            }
+        }
+    }
+}
