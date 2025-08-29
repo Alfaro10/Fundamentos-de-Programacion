794f5e5de6d75b0dfcbf5d513009fe8c17911c69
Alfaro10 <isabel.alfaro@upb.edu.co>
2025-08-29 09:43:52 -0500

    Add files via upload

---
+++ 10.While2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario.
+
+            int número;
+            int factorial = 1;
+            int acumulador;
+
+            Console.WriteLine("Ingrese un número para sacarle factorial");
+            número = Convert.ToInt32(Console.ReadLine());
+
+            acumulador = número;
+
+            while(acumulador > 0) 
+            {
+                factorial *= acumulador;
+                acumulador--;
+            }
+
+            Console.WriteLine($"Factorial de {número} = {factorial}");
+        }
+    }
+}
