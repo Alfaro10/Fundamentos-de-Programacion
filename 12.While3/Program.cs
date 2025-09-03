184f916d6c0e0951dd47f1a6f91e4a26d84217a2
Alfaro10 <isabel.alfaro@upb.edu.co>
2025-09-03 08:24:47 -0500

    Add files via upload

---
+++ 12.While3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _12.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.
+
+            int contador = 1;
+            int suma = 0;
+
+            while (contador <= 99)
+            {
+                suma += contador;
+                Console.WriteLine($"{contador} - {suma}");
+                contador += 2;
+            }
+
+            Console.WriteLine($"La suma de los números impares hasta el 99 es: {suma}");
+
+        }
+    }
+}
