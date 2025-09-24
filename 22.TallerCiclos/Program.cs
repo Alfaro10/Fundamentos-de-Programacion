2dc6c1349c0f8077e97878a8b8303b35de808870
Alfaro10 <isabel.alfaro@upb.edu.co>
2025-09-24 08:13:19 -0500

    Add files via upload

---
+++ 22.TallerCiclos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+
+namespace _22.TallerCiclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1.Algoritmo que permita calcular el promedio de calificaciones, el algoritmo
+            le permitirá al usuario, introducir tantas calificaciones como así desee,
+            en el momento en que seleccione que no desea continuar capturando
+            calificaciones, el algoritmo debe presentar el promedio de las
+            calificaciones capturadas previamente.*/
+
+            /*
+            char ingresarNota = ' ';
+            int contador = 0;
+            float acumulador = 0;
+            float nota = 0;
+
+            Console.WriteLine("Ingresar una calificación? S: si, N: no.");
+            ingresarNota = Convert.ToChar(Console.ReadLine());
+            while (ingresarNota == 'S')
+            {
+                Console.WriteLine($"Ingrese nota {contador}");
+                nota = float.Parse(Console.ReadLine());
+
+                acumulador += nota;
+
+                Console.WriteLine("Ingresar una calificación? S: si, N: no.");
+                ingresarNota = Convert.ToChar(Console.ReadLine());
+
+                contador++;
+            }
+            
+
+            Console.WriteLine($"El promedio de sus notas es {acumulador / contador}");
+            */
+            
+            /* Se requiere un algoritmo para mostrar por pantalla los divisores de un
+            número ingresado por teclado.
+            Tener en cuenta que dados dos números enteros a y b, se dice que b es
+            divisor de a si se cumple que al efectuar una división entera a/b el
+            residuo es 0, en C# utilizar el operador Mod para obtener el residuo de
+            división de dos números.*/
+
+            
+
+        
+        }
+    }
+}
