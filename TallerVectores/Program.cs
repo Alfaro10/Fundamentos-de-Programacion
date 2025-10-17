e06d6f197ac8635d8acc31f98f77c45c88a74d69
Alfaro10 <isabel.alfaro@upb.edu.co>
2025-10-17 09:23:00 -0500

    Add files via upload

---
+++ TallerVectores/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TallerVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector;
+            vector = new int[15];
+            int mayor = 0;
+            int menor = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el número entero para la posición {i + 1}, con índice {i}:");
+                vector[i] = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    mayor = vector[i];
+                    menor = vector[i];
+                }
+                else
+                {
+                    if (vector[i] > mayor)
+                    {
+                        mayor = vector[i];
+                    }
+                    if (vector[i] < menor)
+                    {
+                        menor = vector[i];
+                    }
+                    
+                }
+            
+            }
+            Console.Clear();
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write(vector[i] + "|");
+            }
+
+            Console.WriteLine($"El número mayor es: {mayor}, el número menor es: {menor}");
+        }
+    }
+}
